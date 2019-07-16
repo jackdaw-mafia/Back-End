c37aba6f652d05a49ffb2000a448e8d06105ad8e
Foluso <Owner@s-MacBook-Pro.local>
2019-07-16 18:39:42 +0100

    updated the test file for actual owner profile keys

--- testDynamo/databaseManager.spec.js
+++ testDynamo/databaseManager.spec.js
@@ -23,8 +23,18 @@ afterAll(() => {
 describe("Test database save and get item", () => {
   const item = {
     itemId: "itemId",
-    name: "itemName",
-    status: "itemStatus"
+    id: "data.id",
+    venueName: "data.venueName",
+    data_type: "data.data_type",
+    duration: "data.duration",
+    price: "data.price",
+    drink: "data.drink",
+    quantity: "data.quantity",
+    type: "data.type",
+    coupon_id: "data.coupon_id",
+    active: "data.active",
+    createdAt: "timestamp",
+    updatedAt: "timestamp"
   };
 
   it("save an item", done => {
@@ -36,8 +46,17 @@ describe("Test database save and get item", () => {
   it("get an item", done => {
     databaseManager.getItem("itemId").then(result => {
       assert.equal(item.itemId, result.itemId);
-      assert.equal(item.name, result.name);
-      assert.equal(item.status, result.status);
+      assert.equal(item.venueName, result.venueName);
+      assert.equal(item.data_type, result.data_type);
+      assert.equal(item.duration, result.duration);
+      assert.equal(item.price, result.price);
+      assert.equal(item.drink, result.drink);
+      assert.equal(item.quantity, result.quantity);
+      assert.equal(item.type, result.type);
+      assert.equal(item.coupon_id, result.coupon_id);
+      assert.equal(item.active, result.active);
+      assert.equal(item.createdAt, result.createdAt);
+      assert.equal(item.updatedAt, result.updatedAt);
       done();
     });
   });
